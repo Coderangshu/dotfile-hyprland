c2de319a4aff3bd657fec8fc9675320c889920a3
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2023-04-10 20:30:30 +0530

    zsh, lf added, minor updates in hyprland, starship

--- .config/hyprSetup/dotclone.sh
+++
@@ -1,5 +0,0 @@
-echo ".cfg" >> .gitignore
-git clone --bare https://github.com/Coderangshu/dotfile-hyprland.git $HOME/.cfg
-alias track='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-track checkout -f
-track config --local status.showUntrackedFiles no
